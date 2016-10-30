753da37dfa3703d95692202f29704263aa9bbe60
Tomas Celizna <tomas.celizna@gmail.com>
2016-10-30 16:34:21 +0100

    update

--- test/cssm-rails_test.rb
+++ test/cssm-rails_test.rb
@@ -2,15 +2,20 @@ require 'test_helper'
 
 describe CSSMRails do
   let(:dir) { Pathname(__FILE__).dirname }
-  let(:filename) { 'test.scss' }
+  let(:filename) { 'test.scssm' }
   let(:css) { dir.join("samples/#{filename}").read }
 
   it 'process CSS' do
     CSSMRails.process(css).must_be_kind_of CSSMRails::Result
   end
 
-  it "process CSS" do
+  it 'process CSS' do
     result = CSSMRails.process(css, from: filename)
-    result.injectable_source.must_equal ".test_title_25Esx { color: red; }\n"
+    result.injectable_source.must_equal ".test_title_199rY { color: red; }\n"
+  end
+
+  it 'generates export tokens' do
+    result = CSSMRails.process(css, from: filename)
+    result.export_tokens.must_equal('title' => 'test_title_199rY')
   end
 end
